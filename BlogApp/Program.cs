6f0d9eeee2de154c97c2722eed8c4ea66a2b2aa5
Tolga Topcu <tolgatpc200478@gmail.com>
2024-11-16 11:17:42 +0300

    Create Repositories

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,21 +1,29 @@
+using BlogApp.Data.Abstract;
+using BlogApp.Data.Concrete;
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<BlogContext>(options=>{
     var config = builder.Configuration;
     var connectionString = config.GetConnectionString("sql_connection");
     options.UseSqlite(connectionString);
 });
+
+builder.Services.AddScoped<IPostRepository,EfPostRepository>();
+builder.Services.AddScoped<ITagRepository,EfTagRepository>();
+
 var app = builder.Build();
+
+app.UseStaticFiles();
 
 SeedData.TestVerileriniDoldu(app);
 
 
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
